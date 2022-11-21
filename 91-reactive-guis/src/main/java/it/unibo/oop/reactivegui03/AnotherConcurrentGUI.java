b1fd798f4d3a4642fedc2c730c06b102a58e8fb5
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2022-11-21 11:58:59 +0100

    Done exercise 91 part 3

--- 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 91-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -12,9 +12,148 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.plaf.basic.BasicComboBoxUI.ItemHandler;
 
 /**
  * Third experiment with reactive gui.
  */
 public final class AnotherConcurrentGUI extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel();
+    private final JButton stop = new JButton("stop");
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+
+    /**
+     * Builds a new CGUI.
+     */
+    public AnotherConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        /*
+         * Create the counter agent and start it. This is actually not so good:
+         * thread management should be left to
+         * java.util.concurrent.ExecutorService
+         */
+        final ButtonsAgent agent = new ButtonsAgent();
+        new Thread(agent).start();
+        new Thread(() -> {
+            try {
+                Thread.sleep(10000);
+                agent.stopCounting();
+                this.up.setEnabled(false);
+                this.down.setEnabled(false);
+                this.stop.setEnabled(false);
+            } catch (InterruptedException ex) {
+                /*
+                 * This is just a stack trace print, in a real program there
+                 * should be some logging and decent error reporting
+                 */
+                ex.printStackTrace();
+            }
+        }).start();
+        /*
+         * Register a listener that stops it
+         */
+        stop.addActionListener((e) -> {
+            agent.stopCounting();
+            up.setEnabled(false);
+            down.setEnabled(false);});
+        up.addActionListener(e -> agent.setUp());
+        down.addActionListener(e -> agent.setDown());
+    }
+
+    /*
+     * The counter agent is implemented as a nested class. This makes it
+     * invisible outside and encapsulated.
+     */
+    private class ButtonsAgent implements Runnable {
+        /*
+         * Stop is volatile to ensure visibility. Look at:
+         * 
+         * http://archive.is/9PU5N - Sections 17.3 and 17.4
+         * 
+         * For more details on how to use volatile:
+         * 
+         * http://archive.is/4lsKW
+         * 
+         */
+        private volatile boolean stop;
+        private volatile boolean goesUp = true;
+        private int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    // The EDT doesn't access `counter` anymore, it doesn't need to be volatile 
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> AnotherConcurrentGUI.this.display.setText(nextText));
+                    if (goesUp) {
+                        this.counter++;
+                    } else {
+                        this.counter--;
+                    }
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    /*
+                     * This is just a stack trace print, in a real program there
+                     * should be some logging and decent error reporting
+                     */
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void setUp() {
+            this.goesUp = true;
+        }
+
+        public void setDown() {
+            this.goesUp = false;
+        }
+    }
+
+
+    private class AutomaticAgent implements Runnable {
+
+        private final static int TEN_SECONDS = 10000;
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(TEN_SECONDS);
+                AnotherConcurrentGUI.this.up.setEnabled(false);
+                AnotherConcurrentGUI.this.down.setEnabled(false);
+                AnotherConcurrentGUI.this.stop.setEnabled(false);
+                
+            
+            } catch (InterruptedException ex) {
+                /*
+                 * This is just a stack trace print, in a real program there
+                 * should be some logging and decent error reporting
+                 */
+                ex.printStackTrace();
+            }
+        }
+        
+    }
 }
